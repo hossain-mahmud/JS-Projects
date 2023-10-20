cf671eade1f7cdf720e021c31289d9f6c89656a1
hossain <hossainmahmud.cse@gmail.com>
2023-10-20 12:08:16 +0600

    added file

---
+++ random password/app.js
@@ -0,0 +1,16 @@
+/* Function to generate combination of password */
+function generatePass() {
+	let pass = '';
+	let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+		'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+
+	for (let i = 1; i <= 8; i++) {
+		let char = Math.floor(Math.random()
+			* str.length + 1);
+        console.log(char)
+		pass += str.charAt(char)
+	}
+	return pass;
+}
+
+console.log(generatePass());
