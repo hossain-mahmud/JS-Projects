406d4d60f9704f93c478c6a3454dfb6e25c5531e
hossain <hossainmahmud.cse@gmail.com>
2023-10-20 12:22:18 +0600

    added

--- random password/app.js
+++ random password/app.js
@@ -4,10 +4,9 @@ function generatePass() {
 	let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
 		'abcdefghijklmnopqrstuvwxyz0123456789@#$';
 
-	for (let i = 1; i <= 8; i++) {
+	for (let i = 1; i <= 12; i++) {
 		let char = Math.floor(Math.random()
-			* str.length + 1);
-        console.log(char)
+			* str.length + 1)
 		pass += str.charAt(char)
 	}
 	return pass;
