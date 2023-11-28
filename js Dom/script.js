f9f7ea0a7dab6d870fdd75d3cd98cecbeba15148
hossain <hossainmahmud.cse@gmail.com>
2023-11-28 16:55:47 +0600

    DOM EventListener

--- js Dom/script.js
+++ js Dom/script.js
@@ -1,9 +1,17 @@
 
-
-
-
-
 // Changing HTML Content
 document.getElementById("p1").innerHTML = "New text!";
 // add style css
 document.getElementById("p2").style.color = "blue";
+
+//JavaScript HTML DOM EventListener
+let button = document.querySelector('.button');
+button.addEventListener('click',()=>{
+   alert("Hi i'm Button")
+})
+
+// let button = document.querySelector('.button');
+// button.addEventListener('click',function(){
+//  alert("hi")
+// }
+// )
